2c832f5c15df3f122d232005ed852e35d4c1bab2
Alexander Uzunov <kaialexander.11@gmail.com>
2025-03-18 19:59:55 +0200

    Adding full Single Page Application code
    Single Page Application with page.

---
+++ MOVIES SPA with PAGE/src/app.js
@@ -0,0 +1,23 @@
+import page from '../node_modules/page/page.mjs';
+import { renderMiddleware } from './middlewares/renderMiddleware.js';
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { homeView } from './views/homeView.js';
+import { loginView } from './views/loginView.js';
+import { logoutView } from './views/logoutView.js';
+import { registerView } from './views/registerView.js';
+import { movieView } from './views/movieView.js';
+
+// Attach middleware handlers:
+page(authMiddleware);
+page(renderMiddleware);
+
+// Attach page handlers:
+page('/', homeView);
+page('/login', loginView);
+page('/logout', logoutView);
+page('/register', registerView);
+page('/movies/:movieId', movieView);
+page('/movies', homeView);
+
+
+page.start();
---
+++ MOVIES SPA with PAGE/src/middlewares/renderMiddleware.js
@@ -0,0 +1,29 @@
+import { render, html } from '../../node_modules/lit-html/lit-html.js';
+import { navigationTemplate } from '../views/navigationView.js';
+
+const root = document.querySelector('#root');
+
+const ctxRender = (ctx, templateResult) => {
+
+    let layout = html`
+
+        <nav>
+            ${navigationTemplate(ctx)}
+        </nav>
+
+        <main>
+            ${templateResult}
+        </main>
+
+    `;
+
+    render(layout, root);
+    //render(navigationView, navElement);
+}
+
+
+export const renderMiddleware = (ctx, next) => {
+    //console.log('render middleware');
+    ctx.render = ctxRender.bind(null, ctx);
+    next();
+}
